8135c3fb67fc3ed5ad34b934f045c683d0f218b6
piterjov <piterjov@gmail.com>
2024-01-23 14:11:18 +0100

    init

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"encoding/json"
+	"net/http"
+	"openai_golang/textgenerator"
+)
+
+type RequestBody struct {
+	OpenAPIKey string `json:"open_api_key"`
+	Query string
+
+}
+
+func generateText(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var reqBody RequestBody
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+
+	responseText, err := textgenerator.GetApiResponse(reqBody.Query, reqBody.OpenAPIKey)
+	fmt.Println(err)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error generating text: %s", err.Error()), http.StatusInternalServerError)
+	return
+	}
+	fmt.Fprintln(w, responseText)
+
+}
+ 
+func main() {
+
+	http.HandleFunc("/generate-text", generateText)
+
+	fmt.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ textgenerator/generator.go
@@ -0,0 +1,115 @@
+package textgenerator
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+type Message struct {
+	Content string `json:"content"`
+}
+// Choice represents a single choice in the OpenAI API response
+type Choice struct {
+	Message Message `json:"message"`
+
+}
+
+type Config struct {
+	ApiEndpoint string `json:apiEndpoint`
+	ApiKey string `json:apiKey`
+}
+
+type ApiResponse struct {
+	Choices []Choice `json:"choices"`
+}
+
+func readConfig(filePath string) (Config, error) {
+	var config Config
+	configFile, err := os.Open(filePath)
+
+	if err != nil {
+		return config, err
+	}
+
+	defer configFile.Close()
+
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&config)
+
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
+
+func GetApiResponse(question string, apiKey string) (string, error) {
+	config, err := readConfig("config.json")
+	apiUrl := config.ApiEndpoint
+
+	if err != nil { 
+		fmt.Println("Error reading config file")
+	}
+	
+ 
+    payload := map[string]interface{}{
+        // "model": "gpt-4-1106-preview", // Replace with the correct model name
+		"model": "gpt-3.5-turbo-1106", // Replace with the correct model name
+        "messages": []map[string]string{
+            {
+                "role": "user",
+                "content": question,
+            },
+        },
+    }
+
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest("POST", apiUrl, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		return "", err
+	}
+
+	// apiKey :=  config.ApiKey
+
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("received non-OK status code %d", resp.StatusCode) // TODO: better error message here
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println("API response: ", string(body))
+
+	var apiResponse ApiResponse
+	err = json.Unmarshal(body, &apiResponse)
+	
+	if err != nil {
+		return "", err
+	}
+
+	if len(apiResponse.Choices) > 0 {
+		return apiResponse.Choices[0].Message.Content, nil
+	}
+	return "", fmt.Errorf("no choices in response")
+}
